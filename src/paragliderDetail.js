9f776174b128cd77bbfb24582e8d792284b3881b
DESKTOP-ACT9QHL\Glenn <glenn@glennpacker.net>
2019-06-30 18:08:16 +0100

    memo with useCallback

--- src/paragliderDetail.js
+++ src/paragliderDetail.js
@@ -1,45 +1,46 @@
 import ImageToggleOnScroll from "./imageToggleOnScroll";
 
-const ParagliderDetail = ({
-  id,
-  name,
-  classA,
-  classB,
-  favorite,
-  description,
-  onHeartFavoriteHandler
-}) => {
-  //console.log(`SpeakerDetail:${id} ${firstName} ${lastName} ${favorite}`);
-  return (
-    <div className="card col-4 cardmin pt-3">
-      <ImageToggleOnScroll
-        className="card-img-top"
-        primaryImg={`/static/paragliders/${name.replace(" ", "")}.jpg`}
-        secondaryImg={`/static/paragliders/${name}A.jpg`}
-        alt="{Name}"
-      />
+const ParagliderDetail = React.memo(
+  ({
+    id,
+    name,
+    classA,
+    classB,
+    favorite,
+    description,
+    onHeartFavoriteHandler
+  }) => {
+    return (
+      <div className="card col-4 cardmin pt-3">
+        <ImageToggleOnScroll
+          className="card-img-top"
+          primaryImg={`/static/paragliders/${name.replace(" ", "")}.jpg`}
+          secondaryImg={`/static/paragliders/${name}A.jpg`}
+          alt="{Name}"
+        />
 
-      <div className="card-body">
-        <h4 className="card-title">
-          <button
-            data-sessionid={id}
-            className={favorite ? "heartredbutton" : "heartdarkbutton"}
-            onClick={e => {
-              onHeartFavoriteHandler(e, !favorite);
-            }}
-          />
-          <span>{name}</span>
-        </h4>
+        <div className="card-body">
+          <h4 className="card-title">
+            <button
+              data-sessionid={id}
+              className={favorite ? "heartredbutton" : "heartdarkbutton"}
+              onClick={e => {
+                onHeartFavoriteHandler(e, !favorite);
+              }}
+            />
+            <span>{name}</span>
+          </h4>
 
-        <span>
-          <strong>Class </strong>
-          {classA ? "A" : "B"}
-        </span>
-        <br />
-        <span>{description}</span>
+          <span>
+            <strong>Class </strong>
+            {classA ? "A" : "B"}
+          </span>
+          <br />
+          <span>{description}</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default ParagliderDetail;
