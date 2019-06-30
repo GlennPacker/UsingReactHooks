c113bb6414310cbcaca3383baa9ff26ef1cbb697
DESKTOP-ACT9QHL\Glenn <glenn@glennpacker.net>
2019-06-30 17:03:17 +0100

    useReducer 2

---
+++ src/paraglidersReducer.js
@@ -0,0 +1,24 @@
+const paraglidersReducer = (state, action) => {
+  const updateFavorite = isFav => {
+    return state.map(item => {
+      if (item.id === action.sessionId) {
+        item.favorite = isFav;
+        return item;
+      }
+      return item;
+    });
+  };
+  switch (action.type) {
+    case "setParaglidersList":
+      return action.data;
+    case "favorite":
+      return updateFavorite(true);
+    case "unfavorite":
+      return updateFavorite(false);
+
+    default:
+      return state;
+  }
+};
+
+export default paraglidersReducer;
